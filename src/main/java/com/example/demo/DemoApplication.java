c9b7dc52f0d9d3de8334ca597a28741c8e5e5f87
Endri <endri.zylali@gmail.com>
2022-02-24 18:32:23 -0500

    Removing the duplicated controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,8 +8,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = "com.example.demo")
@@ -39,14 +37,5 @@ class AppCdsApplicationListener implements ApplicationListener<ApplicationReadyE
             restTemplate.getForEntity("http://localhost:8080/", String.class);
             SpringApplication.exit(ctx, () -> 0);
         }
-    }
-}
-
-@RestController
-class HelloController {
-
-    @GetMapping("/")
-    String hello() {
-        return "Hello World!";
     }
 }
